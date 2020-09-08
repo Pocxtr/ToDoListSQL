8856419ab8f1aaca10a896ddbe9c0eb0345ee6e7
Wei <pocxtr@gmail.com>
2020-09-08 13:12:23 +0800

    add user authentication

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -22,11 +22,29 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
 
   User.findOne({ where: { email } }).then(user => {
     if (user) {
-      console.log('User already exists')
+      errors.push({ message: 'Email 已註冊過!' })
       return res.render('register', {
+        errors,
         name,
         email,
         password,
@@ -47,8 +65,10 @@ router.post('/register', (req, res) => {
   })
 })
 
-router.get('/users/logout', (req, res) => {
-  res.send('logout')
+router.get('/logout', (req, res) => {
+  req.logout()
+  req.flash('success_msg', '成功登出')
+  res.redirect('/users/login')
 })
 
 module.exports = router
